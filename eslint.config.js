c8cfbdaa6dd476982edf1970a954f54dcdb097f5
Hubert L.S. <68784598+hlsitechio@users.noreply.github.com>
2025-06-29 13:11:38 -0400

    chore: improve security hygiene

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
-export default [
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
   {
     ignores: ["dist"],
   },
   {
     files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
       sourceType: "module",
     },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
     rules: {},
-  },
-];
+  }
+);
